f0f2d0d5e27ef348fad86b4941522809969b1cc7
Buth <sereybuth120@gmail.com>
2023-02-06 21:40:15 +0700

    this is commmit test

--- src/guidelines/react/react.tsx
+++ src/guidelines/react/react.tsx
@@ -2,13 +2,8 @@ import React from "react";
 
 const App = () => {
   const index = [1, 2, 2, 3, 3, 3];
-  const string = "12";
 
-  index.map((idx) => (
-    <div key={idx * Math.random()}>
-      {idx} {string}
-    </div>
-  ));
+  return index.map((idx) => <div key={idx * Math.random()}>{idx} test commit</div>);
 };
 
 export default App;
