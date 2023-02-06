6d9f8146f7c1fa6d92eec46e7b0ac5019addab28
Buth <sereybuth120@gmail.com>
2023-02-06 21:50:17 +0700

    new test

--- src/guidelines/react/react.tsx
+++ src/guidelines/react/react.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 const App = () => {
   const index = [1, 2, 2, 3, 3, 3];
-
+  const string = 'abc' 
   return index.map((idx) => <div key={idx * Math.random()}>{idx} test commit</div>);
 };
 
